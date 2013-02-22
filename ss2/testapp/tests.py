5dfb2e0e7f9ec94b8344a9fa925700759c4d4cf2
Pratik Vyas <m@pd.io>
2013-02-22 15:52:05 +0530

    add testapp

---
+++ ss2/testapp/tests.py
@@ -0,0 +1,13 @@
+"""
+This file demonstrates writing tests using the unittest module. These will pass
+when you run "manage.py test".
+
+Replace this with more appropriate tests for your application.
+"""
+
+import nose.tools as nt
+
+class TestFruit(object):
+    def test_nose(self):
+        nt.assert_true(True)
+
